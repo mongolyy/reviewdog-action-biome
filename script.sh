45f14176bef99ac2a0d276d7c3bcdb4d66048aab
skanehira <sho19921005@gmail.com>
2024-03-24 13:12:17 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-. lib/biome.sh
+SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
+
+. "${SCRIPT_DIR}/lib/biome.sh"
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
